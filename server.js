84026496d7851c384d6cdb6c22d984a0b94e55d0
jaymi <jaymiclymens@gmail.com>
2021-05-13 21:24:52 -0700

    add campsiterouter

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const campsiteRouter = require("./routes/campsiteRouter");
 
 const hostname = "localhost";
 const port = 3000;
@@ -7,10 +8,12 @@ const port = 3000;
 const app = express(); //best practice to name this variable for the express method 'app'
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.static(_dirname + "/public"));
+
+app.use("/campsites", campsiteRouter);
+
+app.use(express.static(_dirname + "/public")); //i forgot what i wanted to comment
 
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     res.end("<html><body><h1>This is an Express Server</h1></body></html>");
